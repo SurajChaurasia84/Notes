9ff3bd244ffc13abe0219721f716f0d8732c839b
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-05 00:43:09 +0530

    feat: improve note editor

--- lib/screens/note_editor_screen.dart
+++ lib/screens/note_editor_screen.dart
@@ -17,15 +17,41 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
   late final TextEditingController _titleController;
   late final TextEditingController _contentController;
 
+  int _charCount = 0;
+
   @override
   void initState() {
     super.initState();
     _titleController = TextEditingController(text: widget.note?.title ?? '');
     _contentController = TextEditingController(text: widget.note?.content ?? '');
+    _charCount = _contentController.text.length;
+    _contentController.addListener(_updateCharCount);
+  }
+
+  void _updateCharCount() {
+    setState(() {
+      _charCount = _contentController.text.length;
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    final months = [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ];
+    final month = months[dateTime.month - 1];
+    final day = dateTime.day;
+    int hour = dateTime.hour;
+    final minute = dateTime.minute.toString().padLeft(2, '0');
+    final period = hour >= 12 ? 'PM' : 'AM';
+    hour = hour % 12;
+    if (hour == 0) hour = 12;
+    return '$month $day, $hour:$minute $period';
   }
 
   @override
   void dispose() {
+    _contentController.removeListener(_updateCharCount);
     _titleController.dispose();
     _contentController.dispose();
     super.dispose();
@@ -54,7 +80,14 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
     return Scaffold(
       backgroundColor: scaffoldBg,
       appBar: AppBar(
-        title: Text(widget.note == null ? 'Create Note' : 'Edit Note'),
+        title: Text(
+          widget.note == null ? 'Create Note' : 'Edit Note',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: isDark ? Colors.white : Colors.black87,
+          ),
+        ),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () => Navigator.pop(context),
@@ -77,19 +110,40 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
                 hintText: 'Title',
                 border: InputBorder.none,
                 hintStyle: TextStyle(
-                  fontSize: 22,
+                  fontSize: 18,
                   fontWeight: FontWeight.bold,
                   color: Colors.grey,
                 ),
               ),
               style: TextStyle(
-                fontSize: 22,
+                fontSize: 18,
                 fontWeight: FontWeight.bold,
                 color: isDark ? Colors.white : Colors.black87,
               ),
               textCapitalization: TextCapitalization.sentences,
             ),
             const Divider(height: 1),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  _formatDateTime(widget.note?.timestamp ?? DateTime.now()),
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: isDark ? Colors.grey[500] : Colors.grey[600],
+                  ),
+                ),
+                Text(
+                  '$_charCount characters',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: isDark ? Colors.grey[500] : Colors.grey[600],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
             Expanded(
               child: TextField(
                 controller: _contentController,
@@ -97,12 +151,12 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
                   hintText: 'Start writing...',
                   border: InputBorder.none,
                   hintStyle: TextStyle(
-                    fontSize: 16,
+                    fontSize: 14,
                     color: Colors.grey,
                   ),
                 ),
                 style: TextStyle(
-                  fontSize: 16,
+                  fontSize: 14,
                   color: isDark ? Colors.grey[300] : Colors.black87,
                 ),
                 maxLines: null,
