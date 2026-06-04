d7db7945a7368b7ca39ab18d44887c2323c3bfc9
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-05 00:22:19 +0530

    feat: update task model

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -10,4 +10,22 @@ class Task {
     this.isCompleted = false,
     required this.timestamp,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'isCompleted': isCompleted,
+      'timestamp': timestamp.toIso8601String(),
+    };
+  }
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      isCompleted: json['isCompleted'] as bool,
+      timestamp: DateTime.parse(json['timestamp'] as String),
+    );
+  }
 }
