7c85b0e62d633fc65036beed9edb9a1b14c1e875
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-05 00:22:18 +0530

    feat: update note model

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -10,4 +10,22 @@ class Note {
     required this.content,
     required this.timestamp,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'timestamp': timestamp.toIso8601String(),
+    };
+  }
+
+  factory Note.fromJson(Map<String, dynamic> json) {
+    return Note(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      content: json['content'] as String,
+      timestamp: DateTime.parse(json['timestamp'] as String),
+    );
+  }
 }
